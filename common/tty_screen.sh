482bad99fe86ec2b858f4798ddf25b096a4f85d3
Andrea Merello <andrea.merello@gmail.com>
2016-03-14 10:10:24 +0100

    export a tty write function for other scripts (and use it in uboot script)

--- common/tty_screen.sh
+++ common/tty_screen.sh
@@ -3,6 +3,11 @@ find_screen() {
     screen -ls $1 | grep $1 > /dev/null
 }
 
+tty_put() {
+    #echo -n "$1"
+    screen -S $TARGET_NAME -X stuff "$1"
+}
+
 if ! find_screen $TARGET_NAME; then
     sudo chmod 777 $TTY_DEV
     screen -h 10000 -d -m -S zynq $TTY_DEV $TTY_BAUD
@@ -12,3 +17,5 @@ if ! find_screen $TARGET_NAME; then
     echo "FATAL: cannot attach to serial console"
     exit -1
 fi
+
+export -f tty_put
